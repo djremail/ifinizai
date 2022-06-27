59ad49587cf25413db2be217f1c51edeba480738
djremail <borol.igor@gmail.com>
2022-06-27 12:34:30 +0300

    first

---
+++ js/style.js
@@ -0,0 +1,11 @@
+function toggleMenu(){
+  const menuToggle = document.querySelector('.toggle');
+  const sidebar = document.querySelector('.sidebar');
+  menuToggle.classList.toggle('active');
+  sidebar.classList.toggle('active');
+};
+
+// ==================================
+
+
+
